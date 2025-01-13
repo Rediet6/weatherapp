17e26c1b98d01e2744ed066e0f48f901ef600456
Rediet6 <103461342+Rediet6@users.noreply.github.com>
2025-01-13 18:25:29 -0500

    Coding changes in all components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/Rediet6">Rediet Kebere</a> and is
-        <a href="https://github.com/Rediet6/weatherapp">
-          {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather defaultCity="New York" />
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/Rediet6">Rediet Kebere</a> and is
+          <a href="https://github.com/Rediet6/weatherapp">
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
