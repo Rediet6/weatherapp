56b0de28af5545ac287a154615c773893920424d
Rediet6 <103461342+Rediet6@users.noreply.github.com>
2025-01-12 23:00:34 -0500

    Added footer element in App.js component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/Rediet6" target="_blank">
+          Rediet Kebere
+        </a>{" "}
+        and is
+        <a href="https://github.com/Rediet6/weatherapp" target="_blank">
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
